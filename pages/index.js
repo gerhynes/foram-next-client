bf83fac38d4694f88e6bcb7d3d1b642918db0341
Gerard Hynes <gkhynes@gmail.com>
2022-02-22 21:53:41 +0000

    Add Header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/header";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <meta name="description" content="Fóram | A platform for discussion" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className="h-screen grid place-content-center">
         <h1 className="text-2xl font-bold">Welcome to Fóram</h1>
       </main>

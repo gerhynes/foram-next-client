cd02afbb287b8f5e32a763c75c5a5e9843527340
Gerard Hynes <gkhynes@gmail.com>
2022-04-07 12:35:05 +0100

    Have header reflect user login status

--- components/header.js
+++ components/header.js
@@ -2,18 +2,33 @@ import React from "react";
 import Link from "next/link";
 import Avatar from "./avatar";
 
-export default function Header({ user = {} }) {
+export default function Header({ user }) {
   return (
     <header className="bg-indigo-900 flex justify-between items-center px-4">
       <div></div>
       <Link href="/">
         <a className="block text-lg text-white text-center">Fóram</a>
       </Link>
-      <Link href={`/users/${user.username}`}>
-        <a>
-          <Avatar username={user.username} />
-        </a>
-      </Link>
+      {Object.keys(user).length === 0 ? (
+        <div className="flex gap-2 items-center p-2">
+          <Link href="/register">
+            <a className="inline-flex items-center px-2 text-white bg-slate-500 border-2 border-slate-200 hover:bg-slate-200  hover:text-slate-900 transition">
+              Sign up
+            </a>
+          </Link>
+          <Link href="/login">
+            <a className="inline-flex items-center px-2 text-white bg-slate-500 border-2 border-slate-200 hover:bg-slate-200  hover:text-slate-900 transition">
+              Log in
+            </a>
+          </Link>
+        </div>
+      ) : (
+        <Link href={`/users/${user.username}`}>
+          <a>
+            <Avatar username={user.username} />
+          </a>
+        </Link>
+      )}
     </header>
   );
 }
--- components/layout.js
+++ components/layout.js
@@ -5,13 +5,6 @@ import { UserContext } from "../contexts/UserContext";
 
 export default function Layout({ children }) {
   const { user, setUser } = useContext(UserContext);
-  // useEffect(() => {
-  //   setUser({
-  //     name: "Quincy Lars",
-  //     username: "quince",
-  //     id: "33de6e57-c57c-4451-82b9-b73ae248c672"
-  //   });
-  // }, []);
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-1">
